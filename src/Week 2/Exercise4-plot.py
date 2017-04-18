6bf8d9b2bfae5d1e4f4df813739ce64f2a88c242
angelazoo <angelaezou@gmail.com>
2017-04-18 16:56:50 -0700

    Update Exercise4-plot.py

--- src/Week 2/Exercise4-plot.py
+++ src/Week 2/Exercise4-plot.py
@@ -5,27 +5,31 @@ import sys
 data = sys.argv[1]
 
 with open(data, 'r') as f:
+    # x stores data of locus #, y stores CUI
     x = []
     y = []
+     # parse protein-CUI list
     for line in f:
+        # add protein score and name to data lists
         line = line.rstrip().split('\t')
         line[0] = line[0].rstrip()
         x.append(int(line[0][1:]))
         y.append(float(line[1]))
         xy = dict(zip(x,y))
+    # CUI vs. chr order plot, separate series for ribo and other proteins
     plt.figure(1)
     plt.scatter([x for x in sorted(xy)], [xy[x] for x in sorted(xy)], marker='.',\
                  edgecolors='none')
     plt.title('CUI vs. genes in chromosomal order')
     plt.xlabel('Genes (chromosomal order)')
     plt.ylabel('Codon Usage Index')
-     
+    # CUI in ascending order plot  
     plt.figure(2)
     plt.scatter(range(1, len(y)+1), sorted(y), marker='.', edgecolors='none')
     plt.title('CUI vs. genes in ascending order')
     plt.xlabel('Genes (ascending order)')
     plt.ylabel('Codon Usage Index')
-
+    # CUI histogram plot
     plt.figure(3)
     plt.hist(y)
     plt.title('Histogram of CUIs')
