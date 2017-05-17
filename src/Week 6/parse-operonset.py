de59c385f0a680d2a8a81d9e6a59d15fc270ace1
angelazoo <angelaezou@gmail.com>
2017-05-16 17:20:45 -0700

    Update parse-operonset.py

--- src/Week 6/parse-operonset.py
+++ src/Week 6/parse-operonset.py
@@ -1,14 +1,23 @@
 import sys, re
 
+# GeneProductSet.txt
 genelist = sys.argv[1]
+# OperonSet.txt
 operon = sys.argv[2]
+
+# define dictionaries to hold data
 gene_ids = {}
 operons = {}
 
+# open GeneProductSet.txt
 with open(genelist, 'r') as f1:
+	# read each line
 	for line in f1:
+		# if line is not header line
 		if re.search("^#", line) is None:
+			# tab-separated info on each line
 			line = line.rstrip().split('\t')
+			# for each gene with a locus tag, create k,v pair for gene name and locus tag
 			if len(line) >= 3:
 				gene_ids[line[1]] = line[2]
 
