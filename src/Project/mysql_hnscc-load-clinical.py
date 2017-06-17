ad7f7be275aad70f14df3e2fc519c6f6c3e8e5fd
angelazoo <angelaezou@gmail.com>
2017-06-16 23:12:06 -0700

    Update mysql_hnscc-load-clinical.py

--- src/Project/mysql_hnscc-load-clinical.py
+++ src/Project/mysql_hnscc-load-clinical.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 #connect to mysql
-db=MySQLdb.connect(host="bm185s-mysql.ucsd.edu", user="azou", passwd="Gougou12", db="azou_db")
+db=MySQLdb.connect(host="bm185s-mysql.ucsd.edu", user="azou", passwd=xxxxxxxx, db="azou_db")
 # define cursor object from which to execute queries
 c=db.cursor()
 c.execute("""create table if not exists hnscc_clinical (pat_id varchar(12) not null, hpv enum('Negative', 'Positive') not null, smoking_history varchar(25) not null, pack_years varchar(25) not null, alcohol_history varchar(25) not null, num_drinks varchar(25) not null, key(pat_id)) engine=innodb;""") 
